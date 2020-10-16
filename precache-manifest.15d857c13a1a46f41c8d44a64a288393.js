45bd48843e6fc84f75ecc98917ac704f353f7f94
javierdillon21 <javierdillon21@gmail.com>
2020-10-16 18:05:25 -0500

    first

---
+++ precache-manifest.15d857c13a1a46f41c8d44a64a288393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e263549ba5a38f6a12aa7ff5edeabc24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895d7ca77da421ab8913",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d72c6e657d9ccccd1461",
+    "url": "/static/js/2.2494cc76.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2494cc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895d7ca77da421ab8913",
+    "url": "/static/js/main.e4200d94.chunk.js"
+  },
+  {
+    "revision": "00ad1080c358f9de6b2e",
+    "url": "/static/js/runtime-main.90143665.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
